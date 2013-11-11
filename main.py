1d322465915f5acdb9e1ac645f82e3cf8102e387
leonatefm <chengqizhufm@gmail.com>
2013-11-10 23:28:54 -0500

    user record support & resize window

--- main.py
+++ main.py
@@ -16,36 +16,43 @@
 #
 
 import urllib, os, random, json
-import webapp2, jinja2, logging
+import webapp2, jinja2, logging, facebook
 import jokes, jokesim, recommendations
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
+
+FACEBOOK_APP_ID = "701530443205317"
+FACEBOOK_APP_SECRET = "0eb5f65825ee5b53b1904c6b99f18838"
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
+class record(ndb.Model):
+    userid = ndb.StringProperty()
+    ratings = ndb.JsonProperty()
+    
 def getRandomJokes(number):
     joke_keys = jokesim.sim_critics.keys()
     random_joke_keys = random.sample(joke_keys, number)
-    random_jokes = dict()
+    random_jokes = list()
     for key in random_joke_keys:
-        random_jokes[key] = jokes.jokes[key]
+        random_jokes.append((key, jokes.jokes[key]))
     return random_jokes
     
 def getRecommendations(ratings, number):
-    logging.info(ratings)
+    # logging.info(ratings)
     user_rating = dict()
     for key in ratings:
         user_rating[int(key)] = ratings[key]
     user_ratings = {'current':user_rating}
     rec_list = recommendations.getRecommendedItems(user_ratings, jokesim.sim_critics, 'current')
-    if len(rec_list) > 10:
-        rec_list = rec_list[0:10]
+    if len(rec_list) > number:
+        rec_list = rec_list[0:number]
     
-    logging.info(rec_list)
+    # logging.info(rec_list)
     rec_jokes = list()
     for item in rec_list:
         # rec_jokes[item[1]] = jokes.jokes[item[1]]
@@ -54,27 +61,88 @@ def getRecommendations(ratings, number):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+
+        try:
+            user = facebook.get_user_from_cookie(self.request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+        except:
+            user = None
         
-        random_jokes = getRandomJokes(5)
-        
-        logging.info(random_jokes)
-        logging.info(random_jokes.keys())
-        
-        template_values = {
-            'randomjokes': random_jokes,
-            'jokescount': len(random_jokes),
-        }
+        if user:
+            graph = facebook.GraphAPI(user["access_token"])
+            profile = graph.get_object("me")
+            user_name = profile['name']
+            user_id = profile['id']
+            current_user = {
+                'id': user_id,
+                'name': user_name
+            }
+            
+            user_record = record.get_by_id(user_id)
+            
+            if user_record:
+                ratings = user_record.ratings
+                rec_jokes = getRecommendations(ratings, 5)
+                
+                user_rating = dict()
+                for key in ratings:
+                    user_rating[int(key)] = ratings[key]
+                
+                template_values = {
+                    'jokes': rec_jokes,
+                    'jokescount': len(rec_jokes),
+                    'user': current_user,
+                    'ratings': user_rating,
+                    'round': len(user_rating)/5
+                }
+                
+            else:    
+                random_jokes = getRandomJokes(5)
+                template_values = {
+                    'jokes': random_jokes,
+                    'jokescount': len(random_jokes),
+                    'user': current_user
+                }
+        else:
+
+            random_jokes = getRandomJokes(5)
+            template_values = {
+                'jokes': random_jokes,
+                'jokescount': len(random_jokes),
+            }
+                    
+
         template = JINJA_ENVIRONMENT.get_template('/views/index.html')
         self.response.write(template.render(template_values))
         
     def post(self):
         user_ratings = json.loads(self.request.get('ratings'))
-        rec_jokes = getRecommendations(user_ratings, 10)
+        user_id = self.request.get('userid')
+        
+        if user_id:
+            logging.info(user_id)
+            newrecord = record(id = str(user_id), userid = user_id, ratings = user_ratings)
+            newrecord.put()
+            
+            logging.info(record.get_by_id(user_id))
+            
+        else:
+            logging.info('no id, no database, dude')
+        
+        rec_jokes = getRecommendations(user_ratings, 5)
 
         rec_data = {"rec_jokes":rec_jokes}
         
         self.response.out.write(json.dumps(rec_data))
 
 
-
-app = webapp2.WSGIApplication([('/', MainPage)])
+class CheckUser(webapp2.RequestHandler):
+    def post(self):
+        user_id = self.request.get('userid')
+        
+        if record.get_by_id(user_id):
+            self.response.out.write('Yes')
+        else:
+            self.response.out.write('No')
+            
+            
+app = webapp2.WSGIApplication([('/', MainPage),('/checkuser', CheckUser)])
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
+	
+	resizeWindow = function(){
+		itemwidth = $('#container').width();
+		$('.jokes').children().width(itemwidth);
+		$('.jokes').css('left', -itemwidth*(count-1));
+	}
+	resizeWindow();
+	$(window).resize(function(e) {
+		resizeWindow();
+	});
+	
+	
 	var count = 1;
-	var ratings = {};
-	var round = 0;
+	var round = Object.keys(ratings).length/5;
+	console.log('round:' + round);
 
 	rating_event = function(){
 		var joke_key = parseInt($(this).parent().parent().attr('joke'));
@@ -11,7 +23,7 @@ $(document).ready(function(){
 		console.log(ratings);
 		
 		if(count < jokes_count){
-			$('.jokes').css('left', -900*count);
+			$('.jokes').css('left', -itemwidth*count);
 			$('#progress .bar').width(100*count/jokes_count+'%');
 			$('#progress h3').html(count + ' / ' + jokes_count);
 			count = count + 1;
@@ -23,22 +35,24 @@ $(document).ready(function(){
 			}else{
 				$('#progress h3').html('Recalculating recommendations...');
 			}
-			if(round >0 && jokes_count < 10){
+			if(round >0 && jokes_count < 5){
 				alert("Dude, you have finished all the jokes. Go back to your work now!!");
 				$('.round h2').html('Thanks for using Joke Butler Recommender System.');
+				$('#progress h3').html('100% Completed!');
 				$('.jokes li').remove();
 			}else{
 				setTimeout(function(){
 					$.ajax({
 						type:'POST',
 						url:'/',
-						data: {ratings:JSON.stringify(ratings)}
+						data: {ratings:JSON.stringify(ratings), userid:user_id,}
 					}).done(function(data){
+						console.log(data);
 						rec_data = JSON.parse(data);
 						rec_jokes = rec_data['rec_jokes'];
 						round = round + 1;
 					
-						$('.round h2').html('Round ' + round + ': Top 10 jokes recommended');
+						$('.round h2').html('Round ' + round + ': Top '+jokes_count+' jokes recommended');
 						$('.jokes li').remove();
 						$('.jokes').css('left', 0);
 						$('#progress .bar').width(0);
@@ -46,7 +60,7 @@ $(document).ready(function(){
 						$('#progress h3').html('0 / ' + jokes_count);
 						count = 1;
 						for(var i=0; i<jokes_count; i++){
-							var newline = '<li joke='+rec_jokes[i][0]+'>'+rec_jokes[i][1]+'<ul class="rates"><li class="r1"></li><li class="r2"></li><li class="r3"></li><li class="r4"></li><li class="r5"></li></ul></li>';
+							var newline = '<li joke='+rec_jokes[i][0]+'>'+rec_jokes[i][1]+'<ul class="rates"><li class="r1"><h4>UCK!</h4></li><li class="r2"></li><li class="r3"><h4>Meh.</h4></li><li class="r4"></li><li class="r5"><h4>LMAO</h4></li></ul></li>';
 							$('.jokes').append(newline);
 						}
 						$('.rates li').on('click', rating_event);
